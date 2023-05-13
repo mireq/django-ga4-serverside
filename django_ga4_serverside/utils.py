ff1d5ca95fc304c95b4480d065842cda501c49c0
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-05-13 16:30:19 +0200

    feat: Added user_id generator

--- django_ga4_serverside/utils.py
+++ django_ga4_serverside/utils.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 import contextvars
 import logging
+import random
 from collections import namedtuple
 from importlib import import_module
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 from django.conf import settings
+from django.utils import timezone
 
 
 _context = contextvars.ContextVar('context')
 logger = logging.getLogger(__name__)
 ANALYTICS_EVENTS_KEY = '_analytics'
+COOKIE_NAME = '_uid'
 LastRequest = namedtuple('LastRequest', ['request', 'response'])
 
 
@@ -51,8 +54,23 @@ def get_stored_events() -> List[dict]:
 	return getattr(context.request, ANALYTICS_EVENTS_KEY)
 
 
+def generate_user_id() -> str:
+	rnd = random.randint(0, 4294967296)
+	time = int(timezone.now().timestamp())
+	return f'{rnd}.{time}'
+
+
+def get_or_create_user_id(request) -> Tuple[str, bool]:
+	user_id = request.COOKIES.get(COOKIE_NAME)
+	if user_id is None:
+		return generate_user_id(), True
+	else:
+		return user_id, False
+
+
 def _process_analytics(request, response):
-	print("process")
+	user_id, created = get_or_create_user_id(request)
+	print(user_id, created)
 
 
 def process_analytics(request, response):
