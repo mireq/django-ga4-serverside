66cca293738cf4a1000e24d26b8e928029bb757f
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-05-13 19:17:27 +0200

    feat: Implemented collect call

--- django_ga4_serverside/signals/handlers.py
+++ django_ga4_serverside/signals/handlers.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
+import json
+import logging
+from urllib import request
+from urllib.parse import urlencode
+
+from django.conf import settings
 from django.core.signals import request_finished
 from django.dispatch.dispatcher import receiver
 
 from ..utils import get_context, generate_payload
-from django.template.response import TemplateResponse
+
+
+MEASUREMENT_URL = 'https://www.google-analytics.com/mp/collect?'
+logger = logging.getLogger(__name__)
 
 
 @receiver(request_finished)
@@ -12,3 +21,19 @@ def on_request_finished(sender, **kwargs): #! pylint: disable=unused-argument
 	if context is None:
 		return
 	payload = generate_payload(context)
+	if payload is None:
+		return
+
+	query = urlencode({'measurement_id': settings.GA4_ID, 'api_secret': settings.GA4_SECRET})
+	url = f'{MEASUREMENT_URL}?{query}'
+	payload = json.dumps(payload).encode('utf-8')
+
+	user_agent = context.request.headers.get('User-Agent')
+
+	req = request.Request(url)
+	req.add_header('Content-Type', 'application/json; charset=utf-8')
+	if user_agent is not None:
+		req.add_header('User-Agent', user_agent)
+	result = request.urlopen(req, payload)
+	if result.status < 200 or result.status >= 300:
+		logger.warning("Failed to send event: %s", str(result.status))
